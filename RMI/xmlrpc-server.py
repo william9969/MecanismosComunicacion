823d126c7e40dbe17232a93c564093ed1147c567
jessica-maribel <52540894+jessica-maribel@users.noreply.github.com>
2021-11-14 23:15:28 -0500

    Update xmlrpc-server.py

--- RMI/xmlrpc-server.py
+++ RMI/xmlrpc-server.py
@@ -8,32 +8,27 @@ Created on Tue Nov  9 20:31:55 2021
 from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
 
-# Restrict to a particular path.
+# Restringir a un camino en particular.
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2',)
 
-# Create server
+# Crear servidor
 with SimpleXMLRPCServer(('localhost', 8000),
                         requestHandler=RequestHandler) as server:
     server.register_introspection_functions()
 
-    # Register pow() function; this will use the value of
-    # pow.__name__ as the name, which is just 'pow'.
     print("Server is listening on port 8000 ... ")
     server.register_function(pow)
 
-    # Register a function under a different name
     def adder_function(x, y):
         return x + y
     server.register_function(adder_function, 'add')
 
-    # Register an instance; all the methods of the instance are
-    # published as XML-RPC methods (in this case, just 'mul').
+    # Registre una instancia; todos los métodos de la instancia son
+     # publicado como métodos XML-RPC (en este caso, simplemente 'mul').
     class MyFuncs:
         def mul(self, x, y):
             return x * y
-
     server.register_instance(MyFuncs())
-
-    # Run the server's main loop
+    # Ejecutar el servidor
     server.serve_forever()
